43b759f70baee7673c45d7c681d56959c5ac7388
Elena <nuggetnuggetson@gmail.com>
2019-08-11 17:24:35 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf hello:wsgi_application &
-sudo gunicorn ask.wsgi &
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application &
