53692d5a5cd8437fb6de7879676dc3d1f7d0cad1
Elena <nuggetnuggetson@gmail.com>
2019-08-11 10:25:55 +0500

    Update init.sh
    '&' для запуска в фоновом режиме

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf hello:wsgi_application &
