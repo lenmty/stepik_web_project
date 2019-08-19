3326449e07c39d5cd07cff969fcd2bc43d33c20d
lenmty <nuggetnuggetson@gmail.com>
2019-08-19 11:37:13 +0500

    Models Question and Answer was created

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+		
+	def popular(self):
+		return self.order_by('-rating')
+		
+
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+	likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes_set')
+	
+	objects = QuestionManager()
+	
+	def __str__(self):
+		return self.title
+		
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
--- init.sh
+++ init.sh
@@ -14,3 +14,4 @@ sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf hello:wsgi_application &
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application &
+sudo /etc/init.d/mysql start
